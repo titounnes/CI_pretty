4e3c0c5d2455e8da120c7df4ea07120d0928d3f6
harjito <zano.amrhakim@gmail.com>
2017-10-25 10:49:42 +0700

    menghapus tambahan

--- application/logs/log-2017-10-23.php
+++
@@ -1,6 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2017-10-23 18:53:34 --> 404 Page Not Found: /index
-ERROR - 2017-10-23 18:59:50 --> 404 Page Not Found: /index
-ERROR - 2017-10-23 21:02:26 --> 404 Page Not Found: Operator/index
-ERROR - 2017-10-23 21:54:30 --> 404 Page Not Found: Operator/execute
